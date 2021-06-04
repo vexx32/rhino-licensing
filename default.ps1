263e6755a50c870317d64f3ffbf9285be88c3ad1
Joel Francis <32407840+vexx32@users.noreply.github.com>
2021-06-04 11:43:23 -0400

    (maint) Partial build and versioning fixes
    There's more to do here, but this is a solid step forward to fixing the
    PSake build.
    
    Bump version to 1.4 to avoid version collision with the nuget.org
    version of this lib.

--- default.ps1
+++ default.ps1
@@ -1,12 +1,12 @@
-properties { 
+properties {
   $product_name = "Rhino.Licensing"
   $base_dir = Resolve-Path .
   $lib_dir = "$base_dir\SharedLibs"
   $build_dir = "$base_dir\build"
   $buildartifacts_dir = "$build_dir\"
   $sln_file = "$base_dir\$product_name.sln"
-  $version = "1.3.0.0"
-  $humanReadableversion = "1.3"
+  $version = "1.4.0.0"
+  $humanReadableversion = "1.4"
   $tools_dir = "$base_dir\Tools"
   $release_dir = "$base_dir\Release"
   $uploadCategory = "Rhino-Mocks"
@@ -19,7 +19,7 @@ properties {
   $title = "Rhino Licensing $version"
   $copyright = "Hibernating Rhinos & Ayende Rahien 2004 - 2009"
   $description = "Licensing Framework for .NET"
-} 
+}
 
 include .\psake_ext.ps1
 
@@ -35,15 +35,15 @@ task Nuget-Publish  { #-depends Nuget-Pack
   exec { invoke-expression "$nuget push $release_dir\$product_name.$humanReadableversion.nupkg" }
 }
 
-task Clean { 
+task Clean {
   write-host "lib folder is $lib_dir"
 
-  remove-item -force -recurse $buildartifacts_dir -ErrorAction SilentlyContinue 
-  remove-item -force -recurse $release_dir -ErrorAction SilentlyContinue 
-} 
+  remove-item -force -recurse $buildartifacts_dir -ErrorAction SilentlyContinue
+  remove-item -force -recurse $release_dir -ErrorAction SilentlyContinue
+}
 
-task Init -depends Clean { 
-	
+task Init -depends Clean {
+
 	Generate-Assembly-Info `
 		-file "$base_dir\Rhino.Licensing\Properties\AssemblyInfo.cs" `
 		-title $title `
@@ -52,7 +52,7 @@ task Init -depends Clean {
 		-product $title `
 		-version $version `
 		-copyright $copyright
-		
+
 	Generate-Assembly-Info `
 		-file "$base_dir\Rhino.Licensing.Tests\Properties\AssemblyInfo.cs" `
 		-title $title `
@@ -61,7 +61,7 @@ task Init -depends Clean {
 		-product $title `
 		-version $version `
 		-copyright $copyright
-		
+
 	Generate-Assembly-Info `
 		-file "$base_dir\Rhino.Licensing.AdminTool\Properties\AssemblyInfo.cs" `
 		-title $title `
@@ -70,7 +70,7 @@ task Init -depends Clean {
 		-product $title `
 		-version $version `
 		-copyright $copyright
-		
+
 	Generate-Assembly-Info `
 		-file "$base_dir\Rhino.Licensing.AdminTool.Tests\Properties\AssemblyInfo.cs" `
 		-title $title `
@@ -80,24 +80,22 @@ task Init -depends Clean {
 		-version $version `
 		-clsCompliant "false" `
 		-copyright $copyright
-		
-	new-item $release_dir -itemType directory 
-	new-item $buildartifacts_dir -itemType directory 
-	cp $tools_dir\xunit\*.* $build_dir
-} 
 
-task Compile -depends Init { 
-  exec { msbuild /p:OutDir=$buildartifacts_dir $sln_file  }
-} 
+	new-item $release_dir -itemType directory
+	new-item $buildartifacts_dir -itemType directory
+}
+
+task Compile -depends Init {
+  exec { & dotnet build -o $buildartifacts_dir -c Release $sln_file }
+}
 
 task Test -depends Compile {
   $old = pwd
   cd $build_dir
-  
-  exec { invoke-expression "$xunit $build_dir\Rhino.Licensing.Tests.dll /noshadow" }  
-  exec { invoke-expression "$xunit $build_dir\Rhino.Licensing.AdminTool.Tests.dll /noshadow" }
-  
-  cd $old		
+
+  exec { & dotnet test $sln_file }
+
+  cd $old
 }
 
 task Release -depends Test {
@@ -107,13 +105,13 @@ task Release -depends Test {
 		$build_dir\Rhino.Licensing.xml `
 		license.txt `
 		acknowledgements.txt
-		
+
 	& $tools_dir\zip.exe -9 -A -j `
 		$release_dir\Rhino.Licensing-AdminTool-$humanReadableversion-Build-$env:ccnetnumericlabel.zip `
 		$build_dir\Rhino.Licensing.AdminTool.exe `
 		license.txt `
 		acknowledgements.txt
-		
+
 	if ($lastExitCode -ne 0) {
         throw "Error: Failed to execute ZIP command"
     }
@@ -121,9 +119,9 @@ task Release -depends Test {
 
 task Upload -depend Release {
 	if (Test-Path $uploadScript ) {
-		$log = git log -n 1 --oneline		
+		$log = git log -n 1 --oneline
 		msbuild $uploadScript /p:Category=$uploadCategory "/p:Comment=$log" "/p:File=$release_dir\Rhino.Mocks-$humanReadableversion-Build-$env:ccnetnumericlabel.zip"
-		
+
 		if ($lastExitCode -ne 0) {
 			throw "Error: Failed to publish build"
 		}
